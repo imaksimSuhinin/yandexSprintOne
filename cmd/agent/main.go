b1184c61d97d9a292358efd659a6195ace533432
Maksim.S <123143728+imaksimSuhinin@users.noreply.github.com>
2023-01-20 10:47:53 +0500

    add client

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+	localConst "yandexSprintOne/internal/const"
+	localMetrics "yandexSprintOne/internal/metrics"
+)
+
+func main() {
+	StartClient()
+}
+
+func StartClient() {
+	var metrics = localMetrics.Metrics{}
+	data := setData()
+	client := &http.Client{}
+	err, response := setHeader(localConst.ClientEndpoint, data, client)
+	defer response.Body.Close()
+	readDataFromResponse(err, response)
+	getMetrics(metrics, localConst.ClientEndpoint)
+}
+
+func getMetrics(metrics localMetrics.Metrics, endpoint string) {
+	go metrics.UpdateMetrics(3)
+	go metrics.PostMetrics(endpoint, 5)
+	for {
+		time.Sleep(time.Second)
+	}
+}
+
+func setHeader(endpoint string, data url.Values, client *http.Client) (error, *http.Response) {
+	request, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	// отправляем запрос и получаем ответ
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Статус-код ", response.Status)
+	return err, response
+}
+
+func setData() url.Values {
+	data := url.Values{}
+	reader := localMetrics.Metrics{}.Mallocs
+	s := fmt.Sprintf("%f", reader) // s == "123.456000"
+	data.Set("url", s)
+	return data
+}
+
+func readDataFromResponse(err error, response *http.Response) {
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("data:", string(body))
+}
---
+++ internal/metrics/metrics.go
@@ -0,0 +1,115 @@
+package runtime_loc
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+type gauge float64
+type count int64
+
+type Metrics struct {
+	Alloc,
+	TotalAlloc,
+	LiveObjects,
+	BuckHashSys,
+	Frees,
+	GCCPUFraction,
+	GCSys,
+	HeapAlloc,
+	HeapIdle,
+	HeapInuse,
+	HeapObjects,
+	HeapReleased,
+	HeapSys,
+	LastGC,
+	Lookups,
+	MCacheInuse,
+	MCacheSys,
+	MSpanInuse,
+	MSpanSys,
+	Mallocs,
+	NextGC,
+	NumForcedGC,
+	OtherSys,
+	StackInuse,
+	StackSys,
+	Sys,
+	PauseTotalNs,
+	NumGC,
+	NumGoroutine,
+	RandomValue gauge
+
+	PollCount count
+}
+
+func (m *Metrics) UpdateMetrics(duration int) {
+	//var m Metrics
+	var rtm runtime.MemStats
+	var interval = time.Duration(duration) * time.Second
+	var PollCount = 0
+	m.PollCount = count(PollCount)
+	rand.Seed(time.Now().Unix())
+	m.RandomValue = gauge(rand.Intn(100) + 1)
+	for {
+		<-time.After(interval)
+		PollCount++
+		// Read full mem stats
+		runtime.ReadMemStats(&rtm)
+
+		// Number of goroutines
+		m.NumGoroutine = gauge(runtime.NumGoroutine())
+
+		// Misc memory stats
+		m.Alloc = gauge(rtm.Alloc)
+		m.TotalAlloc = gauge(rtm.TotalAlloc)
+		m.Sys = gauge(rtm.Sys)
+		m.Mallocs = gauge(rtm.Mallocs)
+		m.Frees = gauge(rtm.Frees)
+
+		// Live objects = Mallocs - Frees
+		m.LiveObjects = m.Mallocs - m.Frees
+
+		// GC Stats
+		m.PauseTotalNs = gauge(rtm.PauseTotalNs)
+		m.NumGC = gauge(rtm.NumGC)
+
+		m.PollCount = count(PollCount)
+		rand.Seed(time.Now().Unix())
+		m.RandomValue = gauge(rand.Intn(10000) + 1)
+
+	}
+}
+
+func (mertics *Metrics) PostMetrics(serverAddr string, duration int) {
+
+	var interval = time.Duration(duration) * time.Second
+	for {
+		<-time.After(interval)
+		b, _ := json.Marshal(mertics)
+		var inInterface map[string]float64
+		json.Unmarshal(b, &inInterface)
+
+		for field, val := range inInterface {
+			var uri string
+			if field != "PollCount" {
+				uri = "update/gauge/" + field + "/" + strconv.FormatFloat(val, 'f', -1, 64)
+			} else {
+				uri = "update/counter/" + field + "/" + strconv.FormatFloat(val, 'f', -1, 64)
+			}
+			request, err := http.Post(serverAddr+uri, "text/plain", bytes.NewReader([]byte(strconv.FormatFloat(val, 'f', -1, 64))))
+			if err != nil {
+				log.Fatal(err)
+			}
+			request.Body.Close()
+			fmt.Println(serverAddr + uri)
+		}
+	}
+}
