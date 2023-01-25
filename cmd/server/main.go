9d0b97cbe8144ee2bdef2954f27800a1de70c10e
Maksim.S <123143728+imaksimSuhinin@users.noreply.github.com>
2023-01-25 12:30:46 +0300

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import "net/http"
+
+func main() {
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", HelloWorld)
+	// конструируем свой сервер
+	server := &http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	server.ListenAndServe()
+}
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
