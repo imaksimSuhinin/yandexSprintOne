25bd6b7a897239608c604510d037fae9ae00ea2d
Maksim.S <123143728+imaksimSuhinin@users.noreply.github.com>
2023-01-26 06:33:10 +0300

    Update agent.go

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -4,32 +4,33 @@ import (
 	"errors"
 	"github.com/go-resty/resty/v2"
 	"strconv"
+	"time"
 )
 
 func main() {
-	//client := resty.New()
-	//
-	//client.
-	//	SetRetryCount(3).
-	//	SetRetryWaitTime(10 * time.Second)
-	//
-	//resp, err := client.R().
-	//	SetPathParams(map[string]string{
-	//		"host":  "127.0.0.1",
-	//		"port":  strconv.Itoa(8080),
-	//		"type":  "type",
-	//		"name":  "name",
-	//		"value": "value",
-	//	}).
-	//	SetHeader("Content-Type", "text/plain").
-	//	Post("http://{host}:{port}/")
-	//
-	//if err != nil {
-	//
-	//}
-	//if resp.StatusCode() != 200 {
-	//	errors.New("HTTP Status != 200")
-	//}
+	client := resty.New()
+
+	client.
+		SetRetryCount(3).
+		SetRetryWaitTime(10 * time.Second)
+
+	resp, err := client.R().
+		SetPathParams(map[string]string{
+			"host":  "127.0.0.1",
+			"port":  strconv.Itoa(8080),
+			"type":  "type",
+			"name":  "name",
+			"value": "value",
+		}).
+		SetHeader("Content-Type", "text/plain").
+		Post("http://{host}:{port}/")
+
+	if err != nil {
+
+	}
+	if resp.StatusCode() != 200 {
+		errors.New("HTTP Status != 200")
+	}
 
 	//client := &http.Client{}
 	//var body = []byte(`{"message":"Hello"}`)
@@ -67,19 +68,19 @@ func main() {
 	//	os.Exit(1)
 	//}
 
-	client := resty.New()
-
-	resp, _ := client.R().
-		SetPathParams(map[string]string{
-			"host":  "127.0.0.1",
-			"port":  strconv.Itoa(8080),
-			"type":  "type",
-			"name":  "name",
-			"value": "value",
-		}).
-		SetHeader("Content-Type", "text/plain").
-		Post("http://{host}:{port}/")
-	if resp.StatusCode() != 200 {
-		errors.New("HTTP Status != 200")
-	}
+	//client := resty.New()
+	//
+	//resp, _ := client.R().
+	//	SetPathParams(map[string]string{
+	//		"host":  "127.0.0.1",
+	//		"port":  strconv.Itoa(8080),
+	//		"type":  "type",
+	//		"name":  "name",
+	//		"value": "value",
+	//	}).
+	//	SetHeader("Content-Type", "text/plain").
+	//	Post("http://{host}:{port}/")
+	//if resp.StatusCode() != 200 {
+	//	errors.New("HTTP Status != 200")
+	//}
 }
