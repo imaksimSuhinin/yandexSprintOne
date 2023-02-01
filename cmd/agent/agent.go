16e2aa8efc42c2d35d27c24a096063b0055db852
unknown <123143728+imaksimSuhinin@users.noreply.github.com>
2023-02-01 19:59:47 +0300

    update refresh

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -9,23 +9,34 @@ import (
 
 func main() {
 
+	var metrics loc_metric.Metrics
+	go getRefresh(&metrics)
 	go startClient()
+	go getUpload(&metrics, startClient())
 	os.UpdateOsSignal()
 }
 
-func startClient() {
+func startClient() *http.Client {
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	}
-	var m loc_metric.Metrics
-	refresh := time.NewTicker(2 * time.Second)
+
+	return client
+}
+
+func getUpload(m *loc_metric.Metrics, client *http.Client) {
 	upload := time.NewTicker(10 * time.Second)
+	for {
+		<-upload.C
+		m.PostMetrics(client)
+	}
+}
+
+func getRefresh(m *loc_metric.Metrics) {
+	refresh := time.NewTicker(2 * time.Second)
 
 	for {
 		<-refresh.C
 		m.UpdateMetrics()
-
-		<-upload.C
-		m.PostMetrics(client)
 	}
 }
