af58eba44737035e5bbafaabd0a8ada59d4bc590
imaksimSuhinin <vezunchik1985@yandex.ru>
2023-02-04 18:59:27 +0200

    Update osHandle.go

--- internal/os/osHandle.go
+++ internal/os/osHandle.go
@@ -10,19 +10,12 @@ import (
 var SigChanel = make(chan os.Signal, 1)
 
 func HandleOsSignal(signal os.Signal) {
-	if signal == syscall.SIGTERM {
-		fmt.Println("Got kill signal. ")
-		fmt.Println("Program will terminate now.")
+	switch signal {
+	case syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT:
+		fmt.Printf("Got %s signal\n", signal)
+		fmt.Println("Closing")
 		os.Exit(0)
-	} else if signal == syscall.SIGINT {
-		fmt.Println("Got CTRL+C signal")
-		fmt.Println("Closing.")
-		os.Exit(0)
-	} else if signal == syscall.SIGQUIT {
-		fmt.Println("Got Quit signal")
-		fmt.Println("Closing.")
-		os.Exit(0)
-	} else {
+	default:
 		fmt.Println("Ignoring signal: ", signal)
 	}
 }
