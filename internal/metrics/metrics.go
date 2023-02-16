1b43fd8adcd3ba2616d49670e20b27daa05df7b2
imaksimSuhinin <vezunchik1985@yandex.ru>
2023-02-16 19:15:51 +0300

    Update metrics.go

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/imaksimSuhinin/yandexSprintOne/internal/config"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -98,7 +99,7 @@ func (m *Metrics) UpdateMetrics() *Metrics {
 }
 
 func (m *Metrics) PostMetrics(httpClient *http.Client) error {
-
+	conf := config.New()
 	b, _ := json.Marshal(&m)
 	var inInterface map[string]float64
 	json.Unmarshal(b, &inInterface)
@@ -118,7 +119,7 @@ func (m *Metrics) PostMetrics(httpClient *http.Client) error {
 			mkey = field
 		}
 
-		url := NewURLConnectionString(scheme, host, httpUpdatePath+mtype+"/"+mkey+"/"+mval)
+		url := NewURLConnectionString(scheme, conf.AgentConfig.ServerAddr, httpUpdatePath+mtype+"/"+mkey+"/"+mval)
 		var req, err = http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Add(contentTypeKey, contentTypeText) // добавляем заголовок Accept
 
@@ -175,7 +176,8 @@ func (m *Metrics) PostMetricsJSON(httpClient *http.Client) error {
 
 		statJSON, _ := json.Marshal(OneMetrics)
 		//	fmt.Println(mkey)
-		url := NewURLConnectionString(scheme, host, httpUpdatePath)
+		conf := config.New()
+		url := NewURLConnectionString(scheme, conf.AgentConfig.ServerAddr, httpUpdatePath)
 		var req, err = http.NewRequest(http.MethodPost, url, bytes.NewBuffer(statJSON))
 		req.Header.Add(contentTypeKey, contentTypeJSON)
 		//log.Println(statJSON)
